8da7145a1e2682c0a3109ad547d79b8e41d05244
afiq <afiqrasyidm@gmail.com>
2017-12-03 16:08:20 +0700

    initial commit

--- application/controllers/Webhook.php
+++ application/controllers/Webhook.php
@@ -361,17 +361,14 @@ class Webhook extends CI_Controller {
 				
 		}
 		
-	
-		else{
+		else if($event['message']['text'] === "/help"){
 				$textMessageBuilder1 = new TextMessageBuilder("Silahkan ketik jawaban(ex : A) untuk menjawab, atau ketik /selesai untuk selesai, /skip untuk skip pertanyaan ,/pertanyaan untuk melihat pertanyaan sekarang, /ranking untuk melihat ranking kamu group ini");
-	
 				
 				 
 		
 				return $textMessageBuilder1;
-			
-
 		}
+		
 	}
   
 
